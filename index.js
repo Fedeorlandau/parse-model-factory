94a791bc517da813efb799ec05571bd70980cc48
Federico Orlandau <federico.orlandau@gmail.com>
2017-11-14 14:40:46 +0000

    Add babel for compiling es6

--- index.js
+++ index.js
@@ -1,63 +1,87 @@
-const _ = require('underscore');
-const addMasterKey = (options = {}) =>  _.extend({useMasterKey: true}, options);
-const setIncludes = (query, includes) => {
-    _.map(includes, (include) => query.include(include));
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var addMasterKey = function addMasterKey() {
+  var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  return Object.assign({ useMasterKey: true }, options);
+};
+var setIncludes = function setIncludes(query, includes) {
+  includes.map(function (include) {
+    return query.include(include);
+  });
 };
 
-module.exports = {
-  generate: (modelName, modelProperties = {}, objectProperties = {}) => {
-    const Model = _.extend(Parse.Object.extend(modelName, _.extend(objectProperties,
-      {
-        customFetch: function() {
-          return this.fetch(addMasterKey())
-        }
-      })), modelProperties);
+exports.default = {
+  generate: function generate(modelName) {
+    var modelProperties = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+    var objectProperties = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : {};
 
-    Model._query = function() {
+    var Model = Object.assign(Parse.Object.extend(modelName, Object.assign(objectProperties, {
+      customFetch: function customFetch() {
+        return this.fetch(addMasterKey());
+      }
+    })), modelProperties);
+
+    Model._query = function () {
       return new Parse.Query(this);
     };
 
-    Model._find = function(query, includes = [], limit = 5000) {
+    Model._find = function (query) {
+      var includes = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+      var limit = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 5000;
+
       setIncludes(query, includes);
       query.limit(limit);
       return query.find(addMasterKey());
     };
 
-    Model._get = function (query, objectId, includes = []) {
-      setIncludes(query, includes); 
+    Model._get = function (query, objectId) {
+      var includes = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : [];
+
+      setIncludes(query, includes);
       return query.get(objectId, addMasterKey());
     };
 
-    Model._each = function(query, includes = [], callback) {
+    Model._each = function (query) {
+      var includes = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+      var callback = arguments[2];
+
       setIncludes(query, includes);
       return query.each(callback, addMasterKey());
     };
 
-    Model._count = function(query) {
+    Model._count = function (query) {
       return query.count(addMasterKey());
     };
 
-    Model._first = function(query, includes = []){
+    Model._first = function (query) {
+      var includes = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+
       setIncludes(query, includes);
       return query.first(addMasterKey());
     };
 
-    Model.save = function(object, params = null) {
+    Model.save = function (object) {
+      var params = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+
       return object.save(params, addMasterKey());
     };
 
-    Model.saveAll = function(objects) {
-      return Parse.Object.saveAll(objects, addMasterKey())
+    Model.saveAll = function (objects) {
+      return Parse.Object.saveAll(objects, addMasterKey());
     };
 
-    Model.destroy = function(object) {
+    Model.destroy = function (object) {
       return object.destroy(addMasterKey());
     };
 
-    Model.destroyAll = function(objects) {
+    Model.destroyAll = function (objects) {
       return Parse.Object.destroyAll(objects, addMasterKey());
     };
 
     return Model;
   }
 };
+module.exports = exports["default"];
---
+++ src/index.js
@@ -0,0 +1,62 @@
+const addMasterKey = (options = {}) => Object.assign({ useMasterKey: true }, options);
+const setIncludes = (query, includes) => {
+  includes.map(include => query.include(include));
+};
+
+export default {
+  generate: (modelName, modelProperties = {}, objectProperties = {}) => {
+    const Model = Object.assign(Parse.Object.extend(modelName, Object.assign(objectProperties,
+      {
+        customFetch() {
+          return this.fetch(addMasterKey());
+        },
+      })), modelProperties);
+
+    Model._query = function () {
+      return new Parse.Query(this);
+    };
+
+    Model._find = function (query, includes = [], limit = 5000) {
+      setIncludes(query, includes);
+      query.limit(limit);
+      return query.find(addMasterKey());
+    };
+
+    Model._get = function (query, objectId, includes = []) {
+      setIncludes(query, includes);
+      return query.get(objectId, addMasterKey());
+    };
+
+    Model._each = function (query, includes = [], callback) {
+      setIncludes(query, includes);
+      return query.each(callback, addMasterKey());
+    };
+
+    Model._count = function (query) {
+      return query.count(addMasterKey());
+    };
+
+    Model._first = function (query, includes = []) {
+      setIncludes(query, includes);
+      return query.first(addMasterKey());
+    };
+
+    Model.save = function (object, params = null) {
+      return object.save(params, addMasterKey());
+    };
+
+    Model.saveAll = function (objects) {
+      return Parse.Object.saveAll(objects, addMasterKey());
+    };
+
+    Model.destroy = function (object) {
+      return object.destroy(addMasterKey());
+    };
+
+    Model.destroyAll = function (objects) {
+      return Parse.Object.destroyAll(objects, addMasterKey());
+    };
+
+    return Model;
+  },
+};
