fd04b56bc1ff37fc646240205acff78ca65d22e4
Federico Orlandau <federico.orlandau@gmail.com>
2017-11-15 17:49:52 +0000

    More tests and update readme

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 import request from 'supertest';
-import "babel-polyfill";
+import 'babel-polyfill';
 
 const expressServer = require('./lib/index');
 
-describe('loading express', () => {
+describe('Testing all the model factory methods', () => {
   let server;
   beforeEach((done) => {
     if (!server) {
@@ -24,6 +24,36 @@ describe('loading express', () => {
   it('should find the object list', (done) => {
     request(server)
       .get('/find')
+      .expect(200, done);
+  });
+  it('should get an object from the objects list', (done) => {
+    request(server)
+      .get('/get')
+      .expect(200, done);
+  });
+  it('should count the objects', (done) => {
+    request(server)
+      .get('/count')
+      .expect(200, done);
+  });
+  it('should get the first object', (done) => {
+    request(server)
+      .get('/first')
+      .expect(200, done);
+  });
+  it('should save all the objects', (done) => {
+    request(server)
+      .get('/saveAll')
+      .expect(200, done);
+  });
+  it('should destroy an object', (done) => {
+    request(server)
+      .get('/destroy')
+      .expect(200, done);
+  });
+  it('should destroy all the objects', (done) => {
+    request(server)
+      .get('/destroyAll')
       .expect(200, done);
   });
 });
