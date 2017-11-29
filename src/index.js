783edab5d3db10a823c9615ec765dd2ac97ecb11
jeremypiednoel <jeremy.piednoel@gmail.com>
2017-11-29 13:37:43 -0500

    Regular

--- src/index.js
+++ src/index.js
@@ -16,9 +16,10 @@ export default {
       return new Parse.Query(this);
     };
 
-    Model._find = function (query, includes = [], limit = 5000) {
+    Model._find = function (query, includes = [], limit = 5000, skip = 0) {
       setIncludes(query, includes);
       query.limit(limit);
+      query.skip(skip);
       return query.find(addMasterKey());
     };
 
@@ -55,6 +56,23 @@ export default {
 
     Model.destroyAll = function (objects) {
       return Parse.Object.destroyAll(objects, addMasterKey());
+    };
+
+    // No Master Key
+    // - working with session token
+    // - const option = { sessionToken: req.user.getSessionToken() };
+    Model._getRegular = function (option, query, objectId, includes = []) {
+      setIncludes(query, includes);
+      return query.get(objectId, option);
+    };
+
+    Model._countRegular = function (option, query) {
+      return query.count(option);
+    };
+
+    Model._firstRegular = function (option, query, includes = []) {
+      setIncludes(query, includes);
+      return query.first(option);
     };
 
     return Model;
